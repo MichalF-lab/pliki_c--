7638a192023be7240e060cc314cbf6b39c464630
MichalF-lab <MichalF-lab@users.noreply.github.com>
2020-09-15 20:09:01 +0200

    commit to master

---
+++ for_rysunek.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	str l = "|";
+	str h = "-";
+	cin >> i;
+	for h in range(0,i,2)
+	{
+		for g in range(0,i,2)
+		{
+			cout << l;
+		}
+	}
+}
