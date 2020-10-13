89d59c4f00b6eac8c40ecb8303f6da079e14ca43
MichalF-lab <MichalF-lab@users.noreply.github.com>
2020-10-13 03:23:55 +0200

    commit to master

--- rozkład liczby na czynniki.cpp
+++ rozkład liczby na czynniki.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
-
-int main()
-{
-	int i;
-	cin>>i;
+int funkcja (int i)
+{	
 	do
 	{
 		cout<<i%10<<"  ";
 		i = i/10 ;
 	} while (i>1);
 	return 0;
+}
+
+int main()
+{
+	int i;
+	cin>>i;
+	int rozkald = funkcja (i);
 }
