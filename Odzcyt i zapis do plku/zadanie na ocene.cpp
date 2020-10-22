0a349bf255984b1174e4869e8d2b44279104fee3
MichalF-lab <MichalF-lab@users.noreply.github.com>
2020-10-22 09:41:19 +0200

    commit to master

---
+++ Odzcyt i zapis do plku/zadanie na ocene.cpp
@@ -0,0 +1,28 @@
+#include <fstream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+
+int main()
+{
+    int nr_lini=1, zad_a_suma=0,liczba;
+    string linia;
+    fstream plik1, plik2;
+    plik1.open("dane_1.txt", ios::in);
+    plik2.open("wynik_1.txt", ios::out);
+    while (getline(plik1,linia))
+    {
+    	liczba=atoi(linia.c_str());
+    	for(int i=2;i<liczba;i++)
+    	{
+    		if (liczba % i == 0)
+			{
+				zad_a_suma++;
+				break;
+			}
+		}	
+    }
+    plik2<<"zad 1"<<endl<<zad_a_suma<<endl;
+    nr_lini++;
+}
