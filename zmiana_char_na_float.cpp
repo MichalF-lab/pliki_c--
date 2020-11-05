6b55f9165df5367516e1ac0379603544cc037ef6
MichalF-lab <MichalF-lab@users.noreply.github.com>
2020-11-05 22:34:32 +0100

    commit to master

--- zmiana_char_na_float.cpp
+++ zmiana_char_na_float.cpp
@@ -2,28 +2,37 @@
 #include <iostream>
 using namespace std;
 
-float funkcja(char** tab){
-	int n=5;
-    float** tab2 = new float* [n];
-	for (int i = 0; i < 2; ++i)
-	{
-    	tab2[i] = new float[n];
-        for(int j=0;j<5;j++)
-            tab2[i][j]=float(tab[i][j]); 
+char** funkcja(char** tab, int g, int n) {
+
+    char** tab2 = new char* [n];
+    for (int i = 0; i < n; i++)
+    {
+        tab2[i] = new char[g];
+        for (int j = 0; j < g; j++)
+        {
+            tab2[i][j] = tab[j][i];
+            cout <<" "<< tab2[i][j];
+        }
+        cout << endl;
     }
-    return **tab2;
+    return tab2;
 }
 
-int main(){
-    int parametr = 0;
-	int n = 5;
-	char** tab = new char* [n];
-	for (int i = 0; i < 2; ++i)
-	{
-    	tab[i] = new char[n];
-        for(int j=0;j<5;j++)
-            tab[i][j]=rand();
+int main() {
+    int g = 2;
+    int n = 5;
+    cin >> g >> n;
+    char** tab = new char* [n];
+    for (int i = 0; i < n; ++i)
+    {
+        tab[i] = new char[g];
+        for (int j = 0; j < g; j++)
+        {
+            tab[i][j] = rand();
+            cout << tab[i][j] << " ";
+        }
     }
-    float cos = funkcja(tab);
+    cout << endl;
+    char** cos = funkcja(tab, n, g);
     return 0;
 }
