81eb83ecff83506798de437a02bd3659ca48a16d
MichalF-lab <MichalF-lab@users.noreply.github.com>
2020-11-12 01:56:27 +0100

    commit to master

--- zmiana_char_na_float.cpp
+++ zmiana_char_na_float.cpp
@@ -1,16 +1,16 @@
 #include <cstdlib>
 #include <iostream>
+#include <string>
 using namespace std;
 
-char** funkcja(char** tab, int g, int n) {
+string* funkcja(string** tab, int g, int n) {
 
-    char** tab2 = new char* [n];
+    string* tab2 = new string[n];
     for (int i = 0; i < n; i++)
     {
-        tab2[i] = new char[g];
         for (int j = 0; j < g; j++)
         {
-            tab2[i][j] = tab[j][i];
+            tab2[i].append(tab[j][i]);
             cout <<" "<< tab2[i][j];
         }
         cout << endl;
@@ -22,17 +22,18 @@ int main() {
     int g = 2;
     int n = 5;
     cin >> g >> n;
-    char** tab = new char* [n];
+    string** tab = new string* [n];
     for (int i = 0; i < n; ++i)
     {
-        tab[i] = new char[g];
+        tab[i] = new string[g];
         for (int j = 0; j < g; j++)
         {
             tab[i][j] = rand();
             cout << tab[i][j] << " ";
         }
+        cout << endl;
     }
     cout << endl;
-    char** cos = funkcja(tab, n, g);
+    string* cos = funkcja(tab, n, g);
     return 0;
 }
