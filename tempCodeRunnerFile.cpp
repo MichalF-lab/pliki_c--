8b0299b9b9996b8176b1db12567ea5bf07f3fa91
MichalF-lab <MichalF-lab@users.noreply.github.com>
2020-11-12 15:02:09 +0100

    commit to master

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,50 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+bool** funkcja(float** tab1,float** tab2, int n, int m) {
+    bool** tab = new bool* [n];
+    for (int i = 0; i < n; ++i) tab[i] = new bool[m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (tab1[i][j]==tab2[i][j]) tab[i][j]=true;
+            else tab[i][j]=false;
+            cout<<tab[i][j]<<" ";
+        }
+        cout << endl;
+    }
+    return tab;
+}
+
+int main() {
+    int n = 5;
+    int m = 4;
+    float** tab1 = new float* [n];
+    for (int i = 0; i < n; ++i)
+    {
+        tab1[i] = new float[m];
+        for (int j = 0; j < m; j++)
+        {
+            tab1[i][j] = rand();
+            cout << tab1[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    float** tab2 = new float* [n];
+    for (int i = 0; i < n; ++i)
+    {
+        tab2[i] = new float[m];
+        for (int j = 0; j < m; j++)
+        {
+            tab2[i][j] = rand();
+            cout << tab2[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    bool** cos = funkcja(tab1, tab2, n, m);
+    return 0;
+}
